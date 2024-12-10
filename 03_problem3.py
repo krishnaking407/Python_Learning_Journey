1935923059648c47c744eb2a9eb747671aada0df
Krishna Bhagat <156424387+krishnaking407@users.noreply.github.com>
2024-12-10 21:54:12 +0530

    Delete 03_problem3.py

--- 03_problem3.py
+++
@@ -1,4 +0,0 @@
-# check that a tupple type cannot be change in python 
-
-a = (24 , 758 , "krishna")       # i mad ea tupple
-a[2] = "radha"                   #and now i change no. 2 in a string then it cant be change beacouse tuple is inmutual. we cant change it
