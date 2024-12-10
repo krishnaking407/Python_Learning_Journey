a13e72d5367e150f2612f3c97f761774503d7610
Krishna Bhagat <156424387+krishnaking407@users.noreply.github.com>
2024-12-10 21:57:21 +0530

    Add files via upload

---
+++ Chapter 4 ps/02_problem1.py
@@ -0,0 +1,20 @@
+# write a programto axcept the marks of 6 students and display them in a shorted manner 
+
+Marks = []
+
+f1 = int(input("Enter Marke hear :"))
+Marks.append(f1)
+f2 = int(input("Enter Marke hear :"))
+Marks.append(f2)
+f3 = int(input("Enter Marke hear :"))
+Marks.append(f3)
+f4 = int(input("Enter Marke hear :"))
+Marks.append(f4)
+f5= int(input("Enter Marke hear :"))
+Marks.append(f5)
+f6 = int(input("Enter Marke hear :"))
+Marks.append(f6)
+
+Marks.sort()
+
+print(Marks)
---
+++ Chapter 4 ps/05_problem5.py
@@ -0,0 +1,7 @@
+# write a program to find 0 in following tuple 
+
+a = (7, 0, 0, 0, 0, 8, 0, 0, 9)
+
+n = a.count(0)
+
+print(n)
